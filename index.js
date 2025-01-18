079712520eb8f1aef03629204d5e8845abe9b890
oxahex <semanticground@gmail.com>
2025-01-18 18:35:24 +0900

    feat: 캔버스 이미지 2 블렌딩 구현

--- index.js
+++ index.js
@@ -104,6 +104,7 @@
             imagePath: ["./assets/image/eclipse.jpg", "./assets/image/moon.jpg"],
             rectLeftX: [0, 0, { start: 0, end: 0 }],
             rectRightX: [0, 0, { start: 0, end: 0 }],
+            blendHeight: [0 , 0, { start: 0, end: 0 }],
             rectStartY: 0,
         }
     }];
@@ -415,8 +416,21 @@
 
                 } else {
                     // 캔버스 이미지 1이 상단에 닿은 후
-                    // 이미지 블랜드 처리
                     step = 2;
+                    // 이미지 블랜드 처리
+                    // [0, 0, { start: 0, end: 0 }]
+                    values.blendHeight[0] = 0;
+                    values.blendHeight[1] = objs.canvas.height;
+                    values.blendHeight[2].start = values.rectLeftX[2].end;    // 블렌드 애니메이션 시작 시점
+                    values.blendHeight[2].end = values.blendHeight[2].start + 0.2;  // 블렌드 애니메이션 종료 시점
+
+                    const blendHeight = calcValues(values.blendHeight, currentYOffset);
+                    objs.context.drawImage(
+                        objs.images[1],
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+                        0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
+                    );
+
                     // 캔버스 이미지 1 sticky 처리
                     objs.canvas.classList.add("sticky-canvas");
                     objs.canvas.style.top = `-${(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
